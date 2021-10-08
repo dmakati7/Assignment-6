bfa9a7b741af5d8efe7a95bc5ebdc5d7fac598fa
dmakati7 <dmakati2212@sdsu.edu>
2021-10-07 17:55:02 -0700

    coin_flip commit

--- Larger_or_smaller/01-larger-or-smaller.js
+++ Larger_or_smaller/01-larger-or-smaller.js
@@ -7,10 +7,10 @@ firstNumber= parseInt(window.prompt("Enter A"), 10);
 secondNumber= parseInt(window.prompt("Enter B"), 10);
 
 if (firstNumber > secondNumber) {
-    window.document.write("A:" + "" + firstNumber + "" + "is greater.")
+    window.document.write("A:" + " " + firstNumber + " " + "is greater.")
 }
 else if (firstNumber < secondNumber) {
-    window.document.write("B:" + "" + secondNumber + "" + "is greater.")
+    window.document.write("B:" + " " + secondNumber + " " + "is greater.")
 }
 else {
     window.document.write("Both number are equal, A=B")
